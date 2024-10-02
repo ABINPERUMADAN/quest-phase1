239a7b2b0922f14381dacdcb9238af3fb6376d69
abinperumadan <abin.anto112@gmail.com>
2024-10-02 12:06:32 +0530

    commit

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -16,19 +16,19 @@ int main() {
 
     discriminant = b * b - 4 * a * c;//To determine nature of roots
 
-    // condition for real and different roots
-    if (discriminant > 0) {
+    
+    if (discriminant > 0) {                            // condition for real and different roots
         root1 = (-b + sqrt(discriminant)) / (2 * a);
         root2 = (-b - sqrt(discriminant)) / (2 * a);
         printf("root1 = %.2lf and root2 = %.2lf\n", root1, root2);
     }
-    // condition for real and equal roots
-    else if (discriminant == 0) {
+    
+    else if (discriminant == 0) {                 // condition for real and equal roots
         root1 = root2 = -b / (2 * a);
         printf("root1 = root2 = %.2lf\n", root1);
     }
-    // if roots are not real
-    else {
+    
+    else {                                    // if roots are not real
         realPart = -b / (2 * a);
         imagPart = sqrt(-discriminant) / (2 * a);
         printf("root1 = %.2lf+%.2lfi and root2 = %.2lf-%.2lfi\n", realPart, imagPart, realPart, imagPart);
